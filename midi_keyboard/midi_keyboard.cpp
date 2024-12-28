7f0234ac678ee39dec0e50aa469e9fdf1ab518aa
Diederik van der Rhee <diederik.vd.rhee@gmail.com>
2024-12-28 20:09:05 +0100

    wip

--- midi_keyboard/midi_keyboard.cpp
+++ midi_keyboard/midi_keyboard.cpp
@@ -35,13 +35,6 @@ int main()
     sleep_ms(1000);
 
     const int Bitmask16Bit[16] = {1 << 0, 1 << 1, 1 << 2, 1 << 3, 1 << 4, 1 << 5, 1 << 6, 1 << 7, 1 << 8, 1 << 9, 1 << 10, 1 << 11, 1 << 12, 1 << 13, 1 << 14, 1 << 15};
-
-    while (true)
-    {
-        printf("Hello world!\n");
-        sleep_ms(1000);
-    }
-
     while (true)
     {
         for (unsigned int triggerPinIndex = 0; triggerPinIndex < mNumberOfTriggerPins; triggerPinIndex++)
